0edfa698e4776c1a2eea50ebb1777ce82d054a85
Risto Valge <risto@bristo.ee>
2017-10-19 14:11:33 +0300

    Add files via upload

---
+++ 05-Parse-Input-Multiply-Divide/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Parse_Input_Multiply_Divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mina olen kalkulaator programm. Ütle mulle 2 numbrit ja mina ütlen Sulle nende korrutise ja jagatise ");
+            Console.WriteLine();
+            Console.Write("Esimene number: ");
+            string esimene = Console.ReadLine();
+            Console.Write("Teine number: ");
+            string teine = Console.ReadLine();
+            Console.WriteLine();
+
+            int NumbriksEsimene = int.Parse(esimene);
+            int NumbrikstTeine = int.Parse(teine);
+
+            Console.Write($"Korrutates : {esimene} ja {teine} on tulemuseks: ");
+            Console.WriteLine(NumbriksEsimene * NumbrikstTeine);
+            Console.Write("Jagades : " + esimene +" ja " + teine+ " on tulemuseks: ");
+            Console.WriteLine(NumbriksEsimene / NumbrikstTeine);
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Press any key to continue ");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 06-If-Number-Random/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_If_Number_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kas number on suurem või väiksem");
+
+            int number = 5;
+
+            Console.WriteLine();
+            Console.Write("kasutaja sisestab numbri: ");
+            
+            string suva = Console.ReadLine();
+            Console.WriteLine();
+
+            int suva1 = int.Parse(suva);
+             if(number < suva1  )
+            {
+                Console.WriteLine("Number on Väiksem ");
+            }
+
+             if (number > suva1)
+            {
+                Console.WriteLine("Number on Suurem ");
+            }
+
+            if (number == suva1)
+            {
+                Console.WriteLine($"Bingo sa valisid numbriks: {suva}  ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to continue ");
+            Console.ReadLine();
+
+        }
+    }
+}
