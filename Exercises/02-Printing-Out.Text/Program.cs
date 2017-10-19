e71bc77546403d64b05027ac86be29df340bb061
opilane <opilane@KLASSA108-16>
2017-10-19 13:18:37 +0300

    no message

---
+++ Exercises/02-Printing-Out.Text/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Printing_Out.Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere");
+            Console.WriteLine();
+            Console.WriteLine("Minu nimi on Risto  ja ma õpin esimest  korda programmeerima!");
+            Console.WriteLine();
+            Console.WriteLine("Kindlasti on võimalik midagi rohkemat, kui lihtsalt teksti printimine");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue \n");
+           // Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises/05-parse-calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_parse_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mina olen kalkulaator programm. Ütle mulle 2 numbrit ja mina ütlen Sulle nende summa ");
+            Console.WriteLine();
+            Console.Write("Esimene number: ");
+            string esimene = Console.ReadLine();
+            Console.Write("Teine number: ");
+            string teine = Console.ReadLine();
+            Console.WriteLine();
+
+            int NumbriksEsimene =  int.Parse(esimene);
+            int NumbrikstTeine = int.Parse(teine);
+
+            Console.Write("Vastus on : " );
+            Console.WriteLine(NumbriksEsimene + NumbrikstTeine);
+
+
+
+            Console.WriteLine("Press any key to continue ");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
