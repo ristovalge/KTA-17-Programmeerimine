6428bf0ecbcbda2e6f62b89c1540b62ab889301e
sirle-PC\kool <risto@bristo.ee>
2017-10-24 20:01:55 +0300

    kodutöö 23.10.2017

---
+++ 23102017/Exercise1/Exercise 5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata");
+            Console.WriteLine();
+
+            Random rnd = new Random();
+            int maagilineNumber = rnd.Next(1, 100);
+            bool correct = false;
+
+            Console.WriteLine();
+
+
+
+            while (!correct)
+
+            {
+                Console.Write("Sisesta number:");
+                String input = Console.ReadLine();
+                int input1 = int.Parse(input);
+
+
+                Console.WriteLine();
+
+                if (maagilineNumber > input1)
+                {
+                    Console.WriteLine("Arvuti valitud number on Suurem. Sina sisestasid: " + input1 + ". Proovu uuesti!");
+                    Console.WriteLine();
+
+                    continue;
+                }
+                if (maagilineNumber < input1)
+                {
+                    Console.WriteLine("Arvuti Valitud number on Väiksem. Sina sisestasid" + input1 + ". Proovu uuesti!");
+                    Console.WriteLine();
+
+
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("Arvasid numbri ära!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ");
+                    Console.WriteLine();
+
+                    break;
+
+
+
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 23102017/Exercise1/Exercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata");
+            Console.WriteLine();
+            Console.Write("Sisesta Number: ");
+            string input = Console.ReadLine();
+            int input1 = int.Parse(input);
+            int maagilineNumber = 50;
+            Console.WriteLine();
+
+
+            if (maagilineNumber > input1)
+            {
+                Console.WriteLine("Arvuti valitud number on Suurem");
+            }
+            if (maagilineNumber < input1)
+            {
+                Console.WriteLine("Arvuti Valitud number on Väiksem");
+            }
+            if (maagilineNumber == input1)
+            {
+                Console.WriteLine("Arvasid numbri ära");
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
