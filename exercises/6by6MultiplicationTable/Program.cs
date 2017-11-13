70b1a667552f1ee87d927e7f4c763b780b1ecf31
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 15:56:54 +0200

    Add 6by6 multiplication table

---
+++ exercises/6by6MultiplicationTable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6by6MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------------------");
+            // print out 6b6 multiplication table using to nested loops.
+            Console.WriteLine($"X\t0\t1\t2\t3\t4\t5\t6\t");
+
+            for (int i = 1; i < 7; i++)
+            //Console.WriteLine($"| {i} | 0 | 1 | 2 | 3 | 4 | 5 | 6 |");
+            {
+
+                for (int j = 1; j < 7; j++)
+                {
+
+                    Console.Write(i * j + "\t");
+
+
+                }
+                Console.Write("\n");
+            }
+
+
+
+
+
+            Console.WriteLine("--------------------------------------------------------------");
+
+            Console.ReadKey();
+
+        }
+    }
+}
