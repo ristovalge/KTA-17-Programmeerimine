98c3487327577aff346c8320b8ba41db3a254c4f
Risto Valge <risto@bristo.ee>
2017-11-17 16:13:00 +0200

    tunnikontroll

---
+++ Tunnitöö/17112017/tunnikontorll.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace tuninkontroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double total = 0;
+
+
+
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Maion ST.");
+            Console.WriteLine("------------------------------------------------------------");
+           
+
+            while (true)
+            {
+
+                Console.Write("Enter price of food item [-1 to quit]: ");
+                string summa = Console.ReadLine();
+               
+               Double value = double.Parse(summa);
+                
+
+                if (value  == -1 )
+                {
+                    break;
+                    
+                     
+                }
+
+                total = total + value;
+
+
+            }
+             double pros = 0.15 * total;
+
+            Console.WriteLine("------------------------------------------------------------");
+
+            Console.WriteLine();
+
+
+
+
+
+            Console.WriteLine($"Subtotal: € {(total):F2}" );
+            Console.WriteLine($"15.00 % Gratuity: € {(pros):f2}" );
+            Console.WriteLine($"Total: € {(total - pros):F2}" );
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
