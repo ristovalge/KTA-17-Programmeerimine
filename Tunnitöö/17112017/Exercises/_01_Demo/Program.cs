e4fc64aba81e3ffeba4192830da287175230fad9
Risto Valge <risto@bristo.ee>
2017-11-17 13:53:48 +0200

    New folders appear

---
+++ Tunnitöö/17112017/Exercises/_01_Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("See on väike programm mis vajab sisendit !");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Kodutööks valmis !!!");
+            Console.ReadKey();
+        }
+    }
+}
