2ec5b7f8ec6fea5c8d0d6e4d174a81f5fdf6938a
Risto Valge <risto@bristo.ee>
2018-02-11 20:52:01 +0200

    Add files via upload

---
+++ hangmanSolution/hangman/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hangman game = new Hangman();
+            game.Play();
+
+            Console.ReadLine();
+        }
+    }
+}
