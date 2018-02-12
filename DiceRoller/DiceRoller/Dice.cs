9c09bff024cb79291b00993aa95dc164667ad2b0
Risto Valge <risto@bristo.ee>
2018-02-12 16:46:22 +0200

    Add files via upload

---
+++ DiceRoller/DiceRoller/Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Dice
+    {
+
+        public int Roll(int sides)
+        {
+
+            Random rnd = new Random();
+
+
+                
+
+            return rnd.Next(1, sides +1);
+        }
+        
+    }
+}
---
+++ DiceRoller/DiceRoller/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int  sum = 0;
+            int value2 = 0;
+
+            Console.WriteLine("> /roll 3d6 2d8");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Dice uus = new Dice();
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                //var roll = rnd.Next(1, 7);
+                int roll= uus.Roll(6);
+                Console.WriteLine("1d6: {0}", roll);
+
+               
+                sum += roll;
+            }
+            for (int j = 0; j < 2; j++)
+            {
+
+               int roll= uus.Roll(8);
+                Console.WriteLine("1d8: {0}", roll);
+                sum += roll;
+            }
+            //Console.WriteLine("1d6: {0}", rnd.Next(1, 7));
+            //Console.WriteLine("1d6: {0}", rnd.Next(1, 7));
+            //Console.WriteLine("1d6: {0}", rnd.Next(1, 7)); 
+            //Console.WriteLine("1d8: {0}", rnd.Next(1, 9));
+            //Console.WriteLine("1d8: {0}", rnd.Next(1, 9));
+
+            Console.WriteLine();
+            Console.WriteLine("Roll total: {0}",sum );
+            Console.WriteLine();
+            Console.WriteLine(">");
+            Console.WriteLine();
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
